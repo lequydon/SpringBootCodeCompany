ae52edbc903dd5885538914273ea8d1155d01eec
lequydon <donle2044@gmail.com>
2020-07-30 17:17:11 +0700

    add data

--- src/main/java/com/laptrinhjavaweb/api/NewAPI.java
+++ src/main/java/com/laptrinhjavaweb/api/NewAPI.java
@@ -1,5 +1,7 @@
 package com.laptrinhjavaweb.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -7,10 +9,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.laptrinhjavaweb.dto.NewDTO;
-
+import com.laptrinhjavaweb.service.INewService;
+@CrossOrigin
 @RestController
 public class NewAPI {
-
+	@Autowired
+	private INewService newService;
 	@PostMapping(value = "/new")
 	public NewDTO createNew(@RequestBody NewDTO model) {
 		return model;
